cd4164009d40bb47ab1baafb139d4655befeacc2
FaYeest <mtbfarrasandi@gmail.com>
2024-04-05 16:20:03 +0700

    changed source

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://FaYeest.github.io',
-  base: '/dayummmm',
+  base: '/dayummmm/src',
   integrations: [react(), tailwind()],
 });
