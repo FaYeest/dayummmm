923829f40aa4cbe0bcd3a36062462d7a5afe965d
Muhammad Tubagus Farrasandi <mtbfarrasandi@gmail.com>
2024-04-05 18:22:02 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://FaYeest.github.io',
   integrations: [react(), tailwind()],
-  base: '/',
+  base: '/daummmm',
   trailingSlash: "always"
 });
